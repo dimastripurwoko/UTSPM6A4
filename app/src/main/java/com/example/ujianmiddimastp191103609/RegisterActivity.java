dc31ddce8b5e1a0a39b41cf25e8fd25b57d99ee9
dimastripurwoko <dimastripurwoko90@gmail.com>
2022-04-25 18:55:31 +0700

    first commit

---
+++ app/src/main/java/com/example/ujianmiddimastp191103609/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.ujianmiddimastp191103609;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
